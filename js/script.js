3a46b79a67c557d1b90f4cb67148f55a4a8dd8e8
Zayah117 <isaiah.baker@simpledove.com>
2016-07-26 21:42:23 -0400

    I now have 2 cats

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
-var clickCounter = $('#times-clicked');
-var timesClicked = 0;
+var Cat = function(name, nameLabel, image, imageView, timesClicked, clickLabel) {
+	this.name = name;
+	this.nameLabel = nameLabel;
+	this.image = image;
+	this.imageView = imageView;
+	this.timesClicked = timesClicked;
+	this.clickLabel = clickLabel;
+};
 
-$('#cat-image').click(function(e) {
-	// console.log("Click!");
+Cat.prototype.clicker = function(timesClicked, clickLabel) {
 	timesClicked += 1;
-	clickCounter.html("Times clicked: " + timesClicked);
+	clickLabel.html("Times clicked: " + timesClicked);
+	return timesClicked;
+};
+
+function initializeKittyCat(kittyCat) {
+	kittyCat.nameLabel.html(kittyCat.name);
+	kittyCat.imageView.attr("src", kittyCat.image);
+}
+
+bob = new Cat('Bob', $('#name'), 'images/cat.jpg', $('#cat-image'), 0, $('#times-clicked'));
+sally = new Cat('Sally', $('#name-2'), 'images/sally.jpg', $('#cat-image-2'), 0, $('#times-clicked-2'));
+cats = [bob, sally];
+
+for (var i = cats.length - 1; i >= 0; i--) {
+	initializeKittyCat(cats[i]);
+}
+
+bob.imageView.click(function(e) {
+	bob.timesClicked = bob.clicker(bob.timesClicked, bob.clickLabel);
+});
+
+sally.imageView.click(function(e) {
+	sally.timesClicked = sally.clicker(sally.timesClicked, sally.clickLabel);
 });
