97932865da5bd0d7fb9d7d810bd36f6cbe21a8d9
Zayah117 <isaiah.baker@simpledove.com>
2016-07-27 00:24:52 -0400

    You can now delete cats

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ var Cat = function(name, image) {
 	this.name = name;
 	this.image = image;
 	this.timesClicked = 0;
+	this.figure = null;
 	this.nameLabel = null;
 	this.imageView = null;
 	this.clickLabel = null;
+	this.deleteButton = null
 };
 
 Cat.prototype.clicker = function(timesClicked, clickLabel) {
@@ -15,11 +17,13 @@ Cat.prototype.clicker = function(timesClicked, clickLabel) {
 
 
 function initializeKittyCat(kittyCat) {
-	$('#main').append('<figure><h4 id="' + kittyCat.name + '-name"></h4><img id="' + kittyCat.name + '-image"><figcaption id="' + kittyCat.name + '-times-clicked">Love points: 0</figcaption></figure>')
+	$('#main').append('<figure id="' + kittyCat.name + '-figure"><h4 id="' + kittyCat.name + '-name"></h4><img id="' + kittyCat.name + '-image"><figcaption id="' + kittyCat.name + '-times-clicked">Love points: 0</figcaption><button id="' + kittyCat.name + '-button">Delete Cat :(</button></figure>')
 
+	kittyCat.figure = $("#" + kittyCat.name + "-figure");
 	kittyCat.nameLabel = $("#" + kittyCat.name + "-name");
 	kittyCat.imageView = $("#" + kittyCat.name + "-image");
 	kittyCat.clickLabel = $("#" + kittyCat.name + "-times-clicked");
+	kittyCat.deleteButton = $("#" + kittyCat.name + "-button");
 
 	kittyCat.nameLabel.html(kittyCat.name);
 	kittyCat.imageView.attr("src", kittyCat.image);
@@ -27,6 +31,10 @@ function initializeKittyCat(kittyCat) {
 	kittyCat.imageView.click(function(e){
 		kittyCat.timesClicked = kittyCat.clicker(kittyCat.timesClicked, kittyCat.clickLabel);
 	})
+
+	kittyCat.deleteButton.click(function() {
+		kittyCat.figure.remove();
+	});
 }
 
 function checkName(name, array){
@@ -40,7 +48,7 @@ $('#button').click(function() {
 	myColor = $('#color-select').val();
 
 
-	if (checkName(myName, cats) === false) {
+	if (checkName(myName, cats) === false && myName != "") {
 		cats.push(myName);
 		window[myName] = new Cat(myName, 'images/colors/' + myColor + '.png');
 		initializeKittyCat(window[myName]);
